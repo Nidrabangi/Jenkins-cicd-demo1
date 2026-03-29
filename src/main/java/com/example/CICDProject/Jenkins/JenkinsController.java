b7fff65304282cc2c4629170d8457a1bae189e5b
Suresh N <nidrabangihardik@gmail.com>
2026-03-29 10:16:39 +0530

    first commit

---
+++ src/main/java/com/example/CICDProject/Jenkins/JenkinsController.java
@@ -0,0 +1,18 @@
+package com.example.CICDProject.Jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class JenkinsController {
+	
+	
+	@GetMapping("/hello")
+	public String getMethodName() {
+		return "Welcome Again";
+	}
+	
+
+}
